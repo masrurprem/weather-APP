0cfa3e74c806dba5e9ae575fd00f981d922b9675
MD. Saimun Rahman <saimunmail@gmail.com>
2025-05-08 02:56:44 +0600

    minor changes for refactor code

--- index.js
+++ index.js
@@ -18,39 +18,28 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "/index.html"));
 });
 
-//route handler for GET request from weather API with async await
 app.get("/weather", async (req, res) => {
-  console.log("request has hit");
-  // necessary particular for API call
   const city = req.query.city;
-  console.log("the city is:", city);
   const apiKey = "232f36b248a6d3f4b730230b5203f015";
-
-  // complete dynamic api url
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-  // fetch weather data via api call and getting response
-  try {
-    const response = await fetch(apiUrl);
-    // check on response
-    if (!response.ok) {
-      res.status(500).send("there was a api call problem");
-    }
-    const weather = await response.json();
-    console.log("the requested data:");
-    console.log(weather); // actual weather data as js object parsed from JSON
-
-    res.json(weather); // sending response to frontend side fetch request
-  } catch (er) {
-    //console.log(er);
-    res.status(404).send("error fetching weather data");
+  const response = await fetch(apiUrl);
+  if (!response.ok) {
+    res.json({
+      status: +response.status,
+      payload: await response.json(),
+    });
   }
+  res.json({
+    status: +response.status,
+    payload: await response.json(),
+  });
 });
 
-// listening to server via port
 app.listen(port, (err) => {
   if (err) {
     console.log("error connecting the server");
+    process.exit(1);
   }
   console.log("listening to the server from port: ", port);
 });
